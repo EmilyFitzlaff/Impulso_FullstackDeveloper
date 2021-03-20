e58bd9ae0731a0a7d1237eb7bd5ed07a7d20f5c4
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2021-03-20 13:23:20 -0300

    Conceitos iniciais JS

--- Introducao_JS/js/main.js
+++ Introducao_JS/js/main.js
@@ -3,3 +3,10 @@ var idade = 22
 
 alert("Bem-vindo " + nome + "! Você tem " + 22 + " anos");
 
+console.log(nome);
+
+var lista = ['maçã', 'pêra', 'laranja'];
+
+lista.push("uva"); // adiciona novo elemento a lista
+
+lista.pop(); // remove o último elemento
